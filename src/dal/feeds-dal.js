85b5205b4165ccacda9b9193fc1baa2c39a284a6
HarishTagadghar <harishtgtg@gmail.com>
2022-06-15 19:36:41 +0530

    date code refactor

--- src/dal/feeds-dal.js
+++ src/dal/feeds-dal.js
@@ -1,3 +1,4 @@
+const { getPastDateByDays } = require("../common/date-common");
 const { Feeds } = require("../models/feeds-model");
 
 async function insertFeed(feedInfo) {
@@ -7,7 +8,7 @@ async function insertFeed(feedInfo) {
 }
 
 async function getFeedsByUserId(userId) {
-    const createdAt = {   "$gte": new Date(new Date() - 7 * 60 * 60 * 24 * 1000)}
+    const createdAt = {$gte : getPastDateByDays(7)}
     const feeds = await Feeds.find({userId , createdAt})
     console.log("result", feeds)
     return feeds
